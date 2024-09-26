5a2f84f5995465db0a2d818bd715aea5fe1e2a8d
Jimenez <CFJimenez@cps.k12.il.us>
2024-09-26 09:15:51 -0500

    https://github.com/Cristal1314/sets_tuples_lists.py/blob/main/sets_tuples_lists.py

--- sets_tuples_lists.py
+++ sets_tuples_lists.py
@@ -26,35 +26,60 @@ print(fruits)
 
 
 
-cars = ["bmw", "maserati","audi","mercedes", "ferrari"]
-print(f"these are lists of {cars}")
-print(f"the first car is {cars[0]}")
+# cars = ["bmw", "maserati","audi","mercedes", "ferrari"]
+# print(f"these are lists of {cars}")
+# print(f"the first car is {cars[0]}")
 
 
-#changing the value of the lists
-cars [0] = "toyota"
-print(f"the first cars is {cars[0]}")
+# #changing the value of the lists
+# cars [0] = "toyota"
+# print(f"the first cars is {cars[0]}")
 
 
-print(f"the last car is {cars[-1]}")
-cars[-1] = "Lamborghini"
-print(f"the lst car is {cars[-1]}")
+# print(f"the last car is {cars[-1]}")
+# cars[-1] = "Lamborghini"
+# print(f"the lst car is {cars[-1]}")
 
 
-#adding a new value to the list
-cars.append("bugatti")
-print(cars)
-cars.remove("maserati")
-print(cars)
+# #adding a new value to the list
+# cars.append("bugatti")
+# print(cars)
+# cars.remove("maserati")
+# print(cars)
 
 
-#looping through the list
-#otherwise called iterating through the list
-for car in cars:
-   #print(len(car))
-   #print(car)
-#    carRequest = input("add a new car please: ")
-#    cars.append(carRequest)
-   print(len(car))
-   print(cars.uppercase())
-   print(cars)
+# #looping through the list
+# #otherwise called iterating through the list
+# for car in cars:
+#    #print(len(car))
+#    #print(car)
+# #    carRequest = input("add a new car please: ")
+# #    cars.append(carRequest)
+#    print(len(car))
+#    print(cars.uppercase())
+#    print(cars)
+   # if len(cars) > 10:
+   #    break
+
+
+#challenge
+#create a list of friends
+# Make sure the initals list is none
+friends = ["Jazmin", "Lesly","Melissa","Flor", "Aleidy"]
+ # add a new friends to the list, add at least 5 friends
+print(friends)
+ # remove a friend
+friends.remove("Aleidy")
+# insert a friends at specific index mabybe 2
+print(friends.append("Junior"))
+# print the list of friends 
+print(friends)
+# loop through the list and print the friends name
+print(friends.sort)
+# see if a particular friend is in the list (Boolean values)
+print(friends)
+# if the list is greater then 10 break the loop
+for friend in friends:
+   print(friends)
+   if len(friends) > 10:
+      break
